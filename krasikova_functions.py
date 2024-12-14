735100721c7e7f9fc5ae17451f3705fdce322c21
So00s <alex.volkovv@yandex.ru>
2024-12-14 23:48:50 +0300

    feat: создана функция 4.2.6.1

--- krasikova_functions.py
+++ krasikova_functions.py
@@ -3,3 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from streamlit_ace import st_ace
 from code_editor import code_editor
+
+# 4.2.6.1. Кратные интегралы Метод ячеек
+def multiple_integrals_cell_method():
+    pass
